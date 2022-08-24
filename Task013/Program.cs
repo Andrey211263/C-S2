632b28b219505d96940e11e539f33e87cd50b961
Andrey <a_mikhaylov@inbox.ru>
2022-08-25 00:36:43 +0300

    Task13

---
+++ Task013/Program.cs
@@ -0,0 +1,17 @@
+﻿//Вывести третью цифру числа
+
+//int count = 0;
+
+Console.WriteLine("Введите число: ");
+string qul = Console.ReadLine();
+
+//определим разрядность числа
+int count = qul.Length;
+Console.WriteLine( "разрядность числа: "+count );
+
+if(count<3) Console.WriteLine( " Третьей цифры нет! " );
+else
+{
+Console.WriteLine( "Третья цифра: "+qul[2] );
+}
+
