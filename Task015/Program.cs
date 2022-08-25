9d845a1f6a12fd4f1b29a9f32fc1105d79a41b91
Andrey <a_mikhaylov@inbox.ru>
2022-08-25 07:25:26 +0300

    Task15

---
+++ Task015/Program.cs
@@ -0,0 +1,15 @@
+﻿
+Console.WriteLine("Введите день недели");
+int count = Convert.ToInt32(Console.ReadLine());
+if(count>0 && count < 8)
+{
+    if(count != 6 && count != 7) Console.Write("Рабочий день");
+    else{
+    Console.WriteLine("Выходной");
+    }
+
+}
+else 
+{
+    Console.Write("Bad");
+}
